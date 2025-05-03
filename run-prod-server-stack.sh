d39d73176b5eb818cae7867ae49df414e27a2ea3
Ryan Gloff <ryan@ryangloff.com>
2025-05-03 16:14:04 -0400

    Added a conditional to create nginx-net and attach to new containers

--- run-prod-server-stack.sh
+++ run-prod-server-stack.sh
@@ -1,4 +1,10 @@
-docker compose up ui grafana --build -d
+NGINX_NETWORK=nginx-net
+
+if ! docker network ls --format '{{.Name}}' | grep -wq "$NGINX_NETWORK"; then
+  docker network create "$NGINX_NETWORK"
+fi
+
+docker compose --env-file prod-stack.env up ui api grafana --build -d
 
 docker network connect nginx-net api
 docker network connect nginx-net ui
